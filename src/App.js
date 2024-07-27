56431d0f420f298071df12afb0f6470864e98151
GuglielmoColombo <guglielmo.colombo@toyota-europe.com>
2024-07-27 14:08:22 +0200

    deplot

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React, { useState } from 'react';
 import SplashScreen from './components/SplashScreen';
 import MainContent from './components/MainContent'; // Your main content component
+import RecordTrip from './components/RecordTrip'; // Your main content component
 import { Navbar, Nav, Container } from 'react-bootstrap';
 
 function App() {
